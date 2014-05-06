dc445b25e39dd7cf920f8ea4609ee23d97b3e22e
KMassicotte <Keelan1013@gmail.com>
2014-05-06 02:51:18 -0700

    Some basics added

---
+++ Assets/Gu/Scripts/Gu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gu : MonoBehaviour {
+    private static Gu instance = null;
+
+    public static Gu Instance {
+        get {return instance;}
+    }
+
+    void Awake(){
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        }
+        else {
+            instance = this;
+        }
+        
+        //Don't destroy the object when loading new scenes.
+        //This will allow our object to persist between scenes.
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
