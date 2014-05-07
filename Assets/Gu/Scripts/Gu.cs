07b36524f3f6fb466805f043df8803739ba3815c
KMassicotte <Keelan1013@gmail.com>
2014-05-07 01:14:10 -0700

    Basic character movement

--- Assets/Gu/Scripts/Gu.cs
+++ Assets/Gu/Scripts/Gu.cs
@@ -3,6 +3,15 @@ using System.Collections;
 
 public class Gu : MonoBehaviour {
     private static Gu instance = null;
+    private CharacterController charController;
+    private float verticalSpeed;
+    private Vector3 movementVector;
+
+    public float gravity;
+    public float speed;
+    public float turnSpeed;
+    private float jumpSpeed;
+
 
     public static Gu Instance {
         get {return instance;}
@@ -12,8 +21,7 @@ public class Gu : MonoBehaviour {
         if (instance != null && instance != this) {
             Destroy(this.gameObject);
             return;
-        }
-        else {
+        } else {
             instance = this;
         }
         
@@ -24,11 +32,23 @@ public class Gu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        charController = this.GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
+        movementVector = transform.forward * Input.GetAxis("Vertical") * speed;
+        if (charController.isGrounded) {
+            //No need to apply gravity
+            verticalSpeed = 0;
+            if (Input.GetKey("space")) {
+                verticalSpeed = jumpSpeed;
+            }
+        }
+
+        verticalSpeed -= gravity * Time.deltaTime;
+        movementVector.y = verticalSpeed;
+        charController.Move(movementVector * Time.deltaTime);
 	}
 }
