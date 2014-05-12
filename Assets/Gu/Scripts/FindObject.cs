f12b0eccd908431ad89c052ee13a85f94cda986a
KMassicotte <Keelan1013@gmail.com>
2014-05-12 01:07:44 -0700

    Script to find nearest object

---
+++ Assets/Gu/Scripts/FindObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FindObject : MonoBehaviour {
+    private RaycastHit hitObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, fwd, out hitObject, 3f)) {
+            print(hitObject.transform.name);
+        }
+	}
+}
