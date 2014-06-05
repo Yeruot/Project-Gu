8c9d95470f94779fa731529b63f8986cb0877dbd
KMassicotte <Keelan1013@gmail.com>
2014-06-04 23:28:39 -0700

    quick comments for push object

--- Assets/Gu/Scripts/PushObject.cs
+++ Assets/Gu/Scripts/PushObject.cs
@@ -9,7 +9,7 @@ public class PushObject : MonoBehaviour {
         gu = Gu.Instance;
     }
 
-	// Update is called once per frame
+	// Fixed Update is called once per physics frame 
 	void FixedUpdate () {
         moveSpeed = gu.gameObject.GetComponent<ThirdPersonController>().GetSpeed();
         Rigidbody body = this.rigidbody;
@@ -20,7 +20,10 @@ public class PushObject : MonoBehaviour {
         ThirdPersonController thirdPersonController = Gu.Instance.GetComponent<ThirdPersonController>();
         Vector3 direction = thirdPersonController.GetDirection();
 
+	//only move the object if the player is also moving
         if (thirdPersonController.IsMoving()) {
+	    //get the diection of the playe and add velocity of the player
+	    //to that block in the same direction as the player
             Vector3 pushDirection = new Vector3(direction.x, 0.0f, direction.z);
             body.velocity = pushDirection * moveSpeed;
         }
