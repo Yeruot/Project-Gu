0d451cc709d7588b9fd03aca18f9b9e5476861fa
KMassicotte <Keelan1013@gmail.com>
2014-06-05 22:40:20 -0700

    quick comments on throwing objects

--- Assets/Gu/Scripts/ThrowObject.cs
+++ Assets/Gu/Scripts/ThrowObject.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ThrowObject : MonoBehaviour {
     private Gu gu;
 
+    //strength at which we throw an object
     public float throwModifier;
 
 	// Use this for initialization
@@ -13,6 +14,9 @@ public class ThrowObject : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //when we throw an object we first want to put it down
+        //then proceed to add the correct velocity to it and
+        //throw it
 	    if(gu.IsCarrying() && Input.GetButtonDown("Action")){
             GameObject objectToThrow = gu.holdingObject;
             gu.PutDown();
