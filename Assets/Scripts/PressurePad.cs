d9bba29d0863614c30d6e036f3d2e2a493b86914
KMassicotte <Keelan1013@gmail.com>
2014-06-12 22:04:54 -0700

    basic pressure pad script

---
+++ Assets/Scripts/PressurePad.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision){
+        print("On Enter");
+    }
+ 
+    void OnCollisionExit(Collision collision){
+        print("On Enter");
+    }
+}
